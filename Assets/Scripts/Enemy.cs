8ec35eb873233c260eaf176f8ef795a4bd85b0fb
Jazzper80 <jskalsbeek@gmail.com>
2019-10-02 17:00:10 +0200

    First commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private int speed = 4;
+
+    [SerializeField]
+    private GameObject _enemy;
+
+	void Update () 
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if(transform.position.y < -5f)
+        {
+            transform.position = new Vector3(Random.Range(8f, -8f), 6.8f, 0);
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if(player != null)
+            {
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _enemy;
+    [SerializeField]
+    private Player _player;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    private bool _stopSpawning = false;
+
+	void Start () 
+    {
+        StartCoroutine(SpawnRoutine());
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    IEnumerator SpawnRoutine()
+    {
+
+        while (_stopSpawning == false)
+        {
+            GameObject newEnemy = Instantiate(_enemy, new Vector3(Random.Range(9f, -9f), 6.4f, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
